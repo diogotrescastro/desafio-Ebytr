0f232b026841914a966c94301985e7d37d6478a7
Diogo Augusto <diogotrescastro@gmai.com>
2021-10-31 19:12:02 -0300

    adicionado o método get na função de fetch para recuperar todas tarefas

--- client/src/services/todoAPI.js
+++ client/src/services/todoAPI.js
@@ -1,5 +1,5 @@
 export function getTodos() {
-  return fetch(`localhost:3000/listar`)
+  return fetch(`localhost:3000/listar`, {method: 'GET'})
     .then((result) => result.json())
     .then((data) => data);
 }
