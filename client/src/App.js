6a0d3436f4d3de7c39995b4e5a05699f4e4e7a38
Diogo Augusto <diogotrescastro@gmai.com>
2021-10-31 20:01:47 -0300

    adicionado botões de filtro e adicionar nova tarefa, e criado componente de tarefa (task) e função fetch para adicionar nova.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getTodos, addTodo } from './services/todoAPI';
+import Task from './components/task';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  function setTodosFromAPI() {
+    getTodos().then(todos => {
+      setTodos(todos);
+    });
+  }
+
+  function addNewTodo() {
+    addTodo().then(setTodosFromAPI())
+  }
+
+  useEffect(() => {
+    setTodosFromAPI();
+  }, []);
+
   return (
     <div className="App">
        <h1> Lista de Tarefas </h1>
+       <div className="filters">
+         <button>Todas Tarefas</button>
+         <button>Pendentes</button>
+         <button>Em Andamento</button>
+         <button>Prontas</button>
+       </div>
+       {todos.map((todo) => {
+         return (
+           <Task
+              task = {todo} />
+         )
+       })}
+       <div className="newtodo">
+          <input type="text" placeholder="Nova Tarefa" />
+          <input type="text" placeholder="Descrição" />
+          <button onClick={addNewTodo}>Adicionar</button>
+       </div>
     </div>
   );
 }
--- client/src/services/todoAPI.js
+++ client/src/services/todoAPI.js
@@ -1,5 +1,15 @@
 export function getTodos() {
-  return fetch(`localhost:3000/listar`, {method: 'GET'})
+  return fetch(`http://localhost:3000/listar`, {method: 'GET'})
     .then((result) => result.json())
     .then((data) => data);
+}
+
+
+export function addTodo() {
+  return fetch(`http://localhost:3000/adicionar`, {
+    method: 'POST',
+    body: {"title": "teste", "description": "teste", "edit": false, "active": true}
+  })
+    .then((result) => result.json())
+    .then((data) => true);
 }
