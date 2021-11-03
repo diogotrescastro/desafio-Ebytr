1bafda80f4db7c1aa76b4f47ea6dec6af6c7e55b
Diogo Augusto <diogotrescastro@gmai.com>
2021-11-03 19:03:00 -0300

    alteração das funções de ordenação para serem executadas pelo lado do cliente

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { getTodos, getTodosSortedByRecents, getTodosSortedByAz, getTodosSortedByZa} from './services/todoAPI';
+import { useState, useEffect, useCallback } from 'react';
+import { getTodos } from './services/todoAPI';
 import Form from './components/Form'
 import Task from './components/Task'
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [filter, setFilter] = useState();
+  const [,updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
+
+  // A função de forçar o update foi encontrada em https://www.ti-enxame.com/pt/javascript/como-forcar-o-componente-renderizar-novamente-com-ganchos-no-react/808436281/
+  // Pois o react não estava rendedizando o filho após funções de ordenação.
+
 
   function setTodosFromAPI() {
     getTodos().then(todos => {
-      setTodos(todos);
-      
+      setTodos(todos);     
     });
   }
 
@@ -20,21 +25,47 @@ function App() {
   }, []);
 
   function setTodosSortRecents() {
-    getTodosSortedByRecents().then(todos => {
-      setTodos(todos);
-    });
+    let todosSorted = todos;
+    const result = todosSorted.sort((a, b) => {
+      if(a.date > b.date) {
+        return -1;
+      } else {
+        return true;
+      }
+    })
+    setTodos(result);
+    forceUpdate();
+    console.log(todos);
   }
 
   function setTodosSortAz() {
-    getTodosSortedByAz().then(todos => {
-      setTodos(todos);
-    });
+    let todosSorted = todos;
+    const result = todosSorted.sort((a, b) => {
+      if(a.title < b.title) {
+        return -1;
+      } else {
+        return true;
+      }
+    })
+    setTodos(result);
+    forceUpdate();
+    console.log(todos);
+    console.log(result);
   }
 
   function setTodosSortZa() {
-    getTodosSortedByZa().then(todos => {
-      setTodos(todos);
-    });
+    let todosSorted = todos;
+    const result = todosSorted.sort((a, b) => {
+      if(a.title > b.title) {
+        return -1;
+      } else {
+        return true;
+      }
+    })
+    setTodos(result);
+    forceUpdate();
+    console.log(todos);
+    console.log(result);
   }
 
   const filteredTodos = filter? todos.filter(todo => todo.status === filter) : todos;
@@ -42,6 +73,7 @@ function App() {
 
   return (
     <div className="App">
+      {console.log(filteredTodos)}
        <h1> Lista de Tarefas </h1>
        <div className="filters">
        <span>Status</span>
@@ -57,9 +89,10 @@ function App() {
          <button onClick={() => setTodosSortAz()}>A-Z</button>
          <button onClick={() => setTodosSortZa()}>Z-A</button>
        </div>
-       {filteredTodos.map((todo) => {
+       {filteredTodos.map((todo,index) => {
+         console.log(todo.title)
          return (
-           <div key={todo._id}>
+           <div key={index}>
            <Task
               task = {todo}
               getTodos = {setTodosFromAPI}
